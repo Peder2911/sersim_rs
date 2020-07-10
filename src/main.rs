203914cd5e6ed7b8c0347d47f4790b9f966148dd
peder2911 <pglandsverk@gmail.com>
2020-07-10 16:16:32 +0200

    Functioning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use std::fmt;
+use rand::Rng;
+
 struct State {
-    a: i32,
-    b: i32
+    a: f32,
+    b: f32
 }
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"a|{}\nb|{}",self.a,self.b)
+    }
+}
+
+trait InRange {
+    fn in_range(self,begin:Self,end:Self)->bool;
+}
+impl InRange for f32 {
+    fn in_range(self,begin:f32, end:f32)->bool{
+        self >=begin && self < end
+    }
+}
+
 
 macro_rules! compose {
     ( $last:expr ) => { $last };
@@ -20,15 +39,15 @@ where
 
 fn grow(s: State)->State{
     State{
-        a: s.a + 1,
-        b: s.b + 1 
+        a: s.a + 1.,
+        b: s.b + 1. 
     }
 }
 
 fn limit(s: State)->State{
     let new_a = match s.a {
-        1..=10 => s.a,
-        _ => 10
+        x if x.in_range(0.,10.) => s.a,
+        _ => 10.0
     };
     State{
         a: new_a,
@@ -36,19 +55,31 @@ fn limit(s: State)->State{
     }
 }
 
+fn mutate(s:State)->State{
+    let r = rand::thread_rng().gen::<f32>();
+    let new_a = s.a * (r*0.01);
+    State {
+        a: new_a,
+        b: s.b
+    }
+}
+
+fn relate(s:State)->State{
+    State {
+        a: s.a * s.b,
+        b: s.b
+    }
+}
+
 fn main() {
-    let chain = compose!(grow,limit); 
-    let mut state = State{a:1, b:1};
+    let chain = compose!(grow,limit,mutate,relate); 
+    let mut state = State{a:1., b:1.};
     let mut it = 1;
     loop {
         state = chain(state);
-        println!("{:?}| {:?}",it,state.a);
-        println!("{:?}| {:?}",it,state.b);
         it += 1;
         if it > 100 {break};
     }
-
-    println!("Final| {:?}",state.a);
-    println!("Final| {:?}",state.b);
+    println!("{}\n",state);
 }
 
